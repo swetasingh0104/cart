1cfab9263e8f88df3af6dd14c55296ef30236a41
Sweta Kumari Singh <swetasingh7076@gmail.com>
2022-11-25 12:54:30 +0530

    added function

--- src/CartItem.js
+++ src/CartItem.js
@@ -6,14 +6,35 @@ class CartItem extends React.Component{
         this.state = {
           price: 999,
           title: 'Mobile Phone',
-          qty: 1,
+          qty: 0,
           img: ''
         }
         // this.increaseQuantity = this.increaseQuantity.bind(this);
       }
       increaseQuantity = () => {
-        console.log('this', this.state);
+        // this.setState({
+        //   qty: this.state.qty+1         //Shallow merging: it allows to make change only specific value of a state
+        // }, () => {});
+
+        this.setState((prevState) =>{
+          return {qty: prevState.qty+1
+          }
+        });
       }
+      decreaseQuantity = () => {
+        // this.setState({
+        //   qty: this.state.qty+1         //Shallow merging: it allows to make change only specific value of a state
+        // }, () => {});
+
+        this.setState((prevState) =>{
+          if(prevState.qty>0)
+          return {qty: prevState.qty-1
+          }
+          else
+          return 0
+        });
+      }
+
     render(){
         const {price, title, qty}= this.state;
         return (
@@ -22,9 +43,9 @@ class CartItem extends React.Component{
                     <img style={styles.image} alt="img" src="https://t4.ftcdn.net/jpg/01/73/81/41/240_F_173814149_lA4Sgl2tPCWeLJaYFUP7U9h0klYykiOL.jpg"/>
                 </div>
                 <div className="right-block">
-                    <div style={ { fontSize: 25 } }>Phone</div>
-                    <div style={ { color: '#777' } }>Rs 999</div>
-                    <div style={ { color: '#777' } }>Qty: 1</div>
+                    <div style={ { fontSize: 25 } }>{title}</div>
+                    <div style={ { color: '#777' } }>{price}</div>
+                    <div style={ { color: '#777' } }>Qty: {qty}</div>
                     <div className="cart-item-actions">
                      {/* Buttons */}
                      <img
@@ -37,6 +58,7 @@ class CartItem extends React.Component{
               alt="decrease"
               className="action-icons"
               src="https://t3.ftcdn.net/jpg/03/73/49/86/240_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg"
+              onClick={this.decreaseQuantity}
             />
             <img
               alt="delete"

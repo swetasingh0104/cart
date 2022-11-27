19ba88682e9db7fc0e89f783e30c8ac02f0c8d72
Sweta Kumari Singh <swetasingh7076@gmail.com>
2022-11-28 05:16:25 +0530

    added firebase and retrieving data from firebase

--- src/App.js
+++ src/App.js
@@ -2,35 +2,37 @@ import React from 'react';
 import Navbar from './Navbar';
 import Cart from './Cart';
 import Total from './Total';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 class App extends React.Component{
 constructor () {
   super();
   this.state = {
-    products: [
-      {
-          price: 999,
-          title: 'Mobile Phone',
-          qty: 1,
-          img: 'https://t4.ftcdn.net/jpg/01/73/81/41/240_F_173814149_lA4Sgl2tPCWeLJaYFUP7U9h0klYykiOL.jpg',
-          id:1
-      },
-      {
-          price: 99,
-          title: 'Watch',
-          qty: 1,
-          img: 'https://tse1.mm.bing.net/th?id=OIP.ioMJsi__9PIQmCv6jDUKIAHaMh&pid=Api&P=0',
-          id:2
-      },
-      {
-          price: 70,
-          title: 'Cover',
-          qty: 1,
-          img: 'https://tse2.mm.bing.net/th?id=OIP.ETxC6F1n4tjGqScOSL7iggHaJQ&pid=Api&P=0',
-          id:3
-      }
-    ]
+    products: [],
+    loading:true
   }
   // this.increaseQuantity = this.increaseQuantity.bind(this);
+}
+
+componentDidMount(){
+  firebase
+  .firestore()
+  .collection('products')
+  .get()
+  .then((snapshot) =>{
+    const products= snapshot.docs.map((doc) =>{
+      const data = doc.data();
+      data["id"]= doc.id;
+      return data;
+    });
+    this.setState({
+      products:products,
+      loading:false
+    })
+  })
+
 }
 handleIncreaseQuantity=(product) => {
 
@@ -83,7 +85,7 @@ getCartTotal = () => {
 
 render(){
 
-const {products} = this.state;
+const {products, loading} = this.state;
 return (
   <React.Fragment>
       <div className="App">
@@ -97,6 +99,7 @@ return (
       onDecreaseQuantity = {this.handleDecreaseQuantity}
       onDeleteProduct = {this.handleDeleteProduct} 
       />
+      {loading && <h1>Loading Data... please wait</h1>}
       <div className='Total'>
       <Total 
         count ={this.getCartTotal()}
--- src/index.js
+++ src/index.js
@@ -3,12 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 import App from './App';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyASWACoio-cQEnSMDlkssc6uzGRoMxcC3E",
+  authDomain: "cart-9e18b.firebaseapp.com",
+  projectId: "cart-9e18b",
+  storageBucket: "cart-9e18b.appspot.com",
+  messagingSenderId: "738973891107",
+  appId: "1:738973891107:web:e1e008157787a8fd19cf96"
+};
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
 
